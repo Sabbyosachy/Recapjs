135a8dcc0c51a5f19d6b6a867f2701ea27aac1ff
Sabbyosachy <subbyosachychakraborty@gmail.com>
2021-09-11 09:10:48 +0600

    first

---
+++ Fundamental-Six.js
@@ -0,0 +1,29 @@
+//1. use let and const 
+
+//2. use to know how to condition if else >,<,===,!==,>=,<=,||,&&
+
+//3. Array[] use ibdex,length,push,pop
+
+//4. for loop ,while loop,for of,for each,for in
+
+
+//5.Function declar 
+/* function a(num1, num2) {
+    let result = num1 + num2;
+    return result;
+}
+console.log(a(10, 12)); */
+
+//6.object 3 ways to know property name 
+
+
+const student = {
+    name: 'susan',
+    age: 16,
+    hobby: ['cricket', 'football', 'ludu']
+}
+const Age = 'age';
+console.log(student);
+console.log(student.hobby); //Direct use via property
+console.log(student['hobby']); //use via string
+console.log(student[Age]); //use via varible name
